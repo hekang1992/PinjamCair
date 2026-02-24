238aafb4519e8bd1ca2c26915055b04e6319e9ea
hekang1992 <hekang19921111@hotmail.com>
2026-02-24 16:58:52 +0800

    lg

---
+++ PinjamCair/Product/Wallet/WalletViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WalletViewController.swift
+//  PinjamCair
+//
+//  Created by hekang on 2026/2/24.
+//
+
+import UIKit
+
+class WalletViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
