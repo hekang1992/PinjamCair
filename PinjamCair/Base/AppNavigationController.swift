c0d66cdc5cc36181780c8e0cf5a169cfdea9df0a
hekang1992 <hekang19921111@hotmail.com>
2026-02-24 13:36:31 +0800

    first commit

---
+++ PinjamCair/Base/AppNavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  AppNavigationController.swift
+//  PinjamCair
+//
+//  Created by hekang on 2026/2/24.
+//
+
+import UIKit
+
+class AppNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PinjamCair/Base/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  PinjamCair
+//
+//  Created by hekang on 2026/2/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
