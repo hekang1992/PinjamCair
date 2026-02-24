688a24aabac872b00e62f3eb93332d93f7578f8d
hekang1992 <hekang19921111@hotmail.com>
2026-02-24 15:49:03 +0800

    sd

---
+++ PinjamCair/Mine/View/MineView.swift
@@ -0,0 +1,20 @@
+//
+//  MineView.swift
+//  PinjamCair
+//
+//  Created by hekang on 2026/2/24.
+//
+
+import UIKit
+
+class MineView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PinjamCair/Order/View/OrderView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderView.swift
+//  PinjamCair
+//
+//  Created by hekang on 2026/2/24.
+//
+
+import UIKit
+
+class OrderView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
