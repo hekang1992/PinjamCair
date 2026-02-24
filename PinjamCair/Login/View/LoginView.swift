8f8eae5007f3275e9edaa4f4cc69f7f35c54d017
hekang1992 <hekang19921111@hotmail.com>
2026-02-24 15:10:58 +0800

    language-mul

--- PinjamCair/Login/View/LoginView.swift
+++ PinjamCair/Login/View/LoginView.swift
@@ -17,12 +17,25 @@ class LoginView: BaseView {
         return headImageView
     }()
     
+    lazy var bgView: UIView = {
+        let bgView = UIView()
+        bgView.layer.cornerRadius = 16
+        bgView.layer.masksToBounds = true
+        bgView.backgroundColor = UIColor.init(hexString: "#FFFFFF")
+        return bgView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(headImageView)
+        addSubview(bgView)
         headImageView.snp.makeConstraints { make in
             make.top.leading.right.equalToSuperview()
             make.height.equalTo(240.pix())
+        }
+        bgView.snp.makeConstraints { make in
+            make.top.equalTo(headImageView).offset(230.pix())
+            make.left.right.bottom.equalToSuperview()
         }
     }
     
