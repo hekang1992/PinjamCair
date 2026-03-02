f8388b4b4e1ec1aea11883e5387bd594eb287b2f
hekang1992 <hekang19921111@hotmail.com>
2026-03-02 16:12:49 +0800

    order-list-ui

--- PinjamCair/Order/Controller/OrderListViewController.swift
+++ PinjamCair/Order/Controller/OrderListViewController.swift
@@ -11,10 +11,18 @@ import RxSwift
 import RxCocoa
 import RxGesture
 import TYAlertController
+import MJRefresh
 
 class OrderListViewController: BaseViewController {
     
     private let viewModel = AppViewModel()
+    
+    private var modelArray: [dipsiauousModel] = []
+    
+    lazy var emptyView: EmptyView = {
+        let emptyView = EmptyView(frame: .zero)
+        return emptyView
+    }()
     
     var name: String? {
         didSet {
@@ -44,6 +52,24 @@ class OrderListViewController: BaseViewController {
         return bgView
     }()
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        tableView.estimatedRowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = true
+        tableView.showsVerticalScrollIndicator = false
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(OrderListViewCell.self, forCellReuseIdentifier: "OrderListViewCell")
+        if #available(iOS 15.0, *) {
+            tableView.sectionHeaderTopPadding = 0
+        }
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -69,7 +95,96 @@ class OrderListViewController: BaseViewController {
             make.top.equalTo(headView.snp.bottom).offset(16)
             make.left.right.bottom.equalToSuperview()
         }
+        
+        bgView.addSubview(emptyView)
+        bgView.addSubview(tableView)
+        
+        emptyView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
                
+    }
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Task {
+            await self.orderListInfo()
+        }
+    }
+    
+}
+
+extension OrderListViewController {
+    
+    private func orderListInfo() async {
+        do {
+            var type: String = "4"
+            if name == LocalStr("All") {
+                type = "4"
+            }else if name == LocalStr("In progress") {
+                type = "7"
+            }else if name == LocalStr("Repayment") {
+                type = "6"
+            }else if name == LocalStr("Finished") {
+                type = "5"
+            }
+            let parameters = ["carcinpickward": type,
+                              "cacohood": "1",
+                              "seatster": "100"]
+            let model = try await viewModel.orderListInfo(parameters: parameters)
+            let ectopurposeess = model.ectopurposeess ?? ""
+            if ["0", "00"].contains(ectopurposeess) {
+                let modelArray = model.casia?.dipsiauous ?? []
+                self.modelArray = modelArray
+                self.tableView.reloadData()
+                if modelArray.isEmpty || modelArray.count == 0 {
+                    self.tableView.isHidden = true
+                    self.emptyView.isHidden = false
+                }else {
+                    self.tableView.isHidden = false
+                    self.emptyView.isHidden = true
+                }
+            }else {
+                self.tableView.isHidden = true
+                self.emptyView.isHidden = false
+            }
+            await self.tableView.mj_header?.endRefreshing()
+        } catch {
+            self.tableView.isHidden = true
+            self.emptyView.isHidden = false
+            await self.tableView.mj_header?.endRefreshing()
+        }
+    }
+    
+}
+
+extension OrderListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.modelArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderListViewCell", for: indexPath) as! OrderListViewCell
+        let model = self.modelArray[indexPath.row]
+        cell.model = model
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let model = self.modelArray[indexPath.row]
+        let pageUrl = model.ciliwhichery ?? ""
+        if pageUrl.contains(SchemeRouter.shared.schemeUrl) {
+            SchemeRouter.shared.handle(urlString: pageUrl, vc: self)
+        }else {
+            let webVc = H5ViewController()
+            webVc.pageUrl = pageUrl
+            self.navigationController?.pushViewController(webVc, animated: true)
+        }
     }
     
 }
